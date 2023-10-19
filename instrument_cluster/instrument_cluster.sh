25426bab146d321c71164d8883bfcc652c1f4a3b
K0Dahyun <gdh1685@kookmin.ac.kr>
2023-10-19 13:58:34 +0200

    modify install /usr/bin/DES3

--- instrument_cluster/instrument_cluster.sh
+++ instrument_cluster/instrument_cluster.sh
@@ -5,5 +5,6 @@ SCRIPT_DIR=$(dirname "$0")
 export COMMONAPI_CONFIG="$SCRIPT_DIR/instrument_cluster.ini"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SCRIPT_DIR/build"
 
-$SCRIPT_DIR/build/instrument_cluster
+# $SCRIPT_DIR/build/instrument_cluster -platform eglfs
+$SCRIPT_DIR/build/instrument_cluster -platform eglfs
 
