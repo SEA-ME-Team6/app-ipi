bee792f2b45803791a44decd9c0d738c013be1e6
K0Dahyun <gdh1685@kookmin.ac.kr>
2023-10-20 11:05:45 +0200

    Add the top of CMakeList.txt

--- instrument_cluster/instrument_cluster.sh
+++ instrument_cluster/instrument_cluster.sh
@@ -6,5 +6,5 @@ export COMMONAPI_CONFIG="$SCRIPT_DIR/instrument_cluster.ini"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SCRIPT_DIR/build"
 
 # $SCRIPT_DIR/build/instrument_cluster -platform eglfs
-$SCRIPT_DIR/build/instrument_cluster -platform eglfs
+$SCRIPT_DIR/build/instrument_cluster
 
