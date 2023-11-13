808a26def7510890531d86a4c2abfd8a3332a59d
K0Dahyun <gdh1685@kookmin.ac.kr>
2023-11-13 14:02:30 +0100

    set routing manager (export VSOMEIP_CONFIGURATION=../vsomeip.json )

--- head_unit/build/CMakeFiles/hu-gearsomeipproxy_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/hu-gearsomeipproxy_autogen"
-  "hu-gearsomeipproxy_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hu-gearsomeipproxy_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
