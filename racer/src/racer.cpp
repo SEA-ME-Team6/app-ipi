60b1757c9b58596c22f180c0e0edfbc61ddd7c95
K0Dahyun <gdh1685@kookmin.ac.kr>
2023-11-19 20:19:48 +0100

    test v23

--- racer/src/racer.cpp
+++ racer/src/racer.cpp
@@ -27,23 +27,14 @@ void Racer::setLight(bool light){
 }
 
 void Racer::set_steering_percent(float steering) {
-    PyGILState_STATE gstate;
-    gstate = PyGILState_Ensure();
-
     std::cout << "Steering : " << steering << std::endl;
     PyObject_CallMethod(pInstance, "set_steering_percent", "(f)", steering);
-
-    PyGILState_Release(gstate);
 }
 
 void Racer::set_throttle_percent(float throttle) {
-    PyGILState_STATE gstate;
-    gstate = PyGILState_Ensure();
-
     std::cout << "Throttle : " << throttle << std::endl;
     PyObject_CallMethod(pInstance, "set_throttle_percent", "(f)", throttle);
 
-    PyGILState_Release(gstate);
     /*
     if (gearStatus == 1 || gearStatus == 3) {
         std::cout << "Throttle : " << throttle << std::endl;
@@ -53,14 +44,10 @@ void Racer::set_throttle_percent(float throttle) {
 }
 
 uint32_t Racer::getBatttery() {
-    PyGILState_STATE gstate;
-    gstate = PyGILState_Ensure();
-
     pVoltage = PyObject_CallMethod(pInstance, "get_battery_voltage", NULL);
     voltage = PyFloat_AsDouble(pVoltage);
     batteryStatus = (uint32_t)((voltage/3-3.1)*100/1.1);
 
-    PyGILState_Release(gstate);
     return batteryStatus;
 }
 
