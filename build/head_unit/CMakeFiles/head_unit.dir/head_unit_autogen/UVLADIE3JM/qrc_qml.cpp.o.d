4325577a4468e8defe5de34566e82b84fba5ee08
K0Dahyun <gdh1685@kookmin.ac.kr>
2023-11-19 18:36:17 +0100

    modify shared library name in ini file

--- build/head_unit/CMakeFiles/head_unit.dir/head_unit_autogen/UVLADIE3JM/qrc_qml.cpp.o.d
+++
@@ -1,3 +0,0 @@
-head_unit/CMakeFiles/head_unit.dir/head_unit_autogen/UVLADIE3JM/qrc_qml.cpp.o: \
- /home/seame-workstation08/DES_Head-Unit/build/head_unit/head_unit_autogen/UVLADIE3JM/qrc_qml.cpp \
- /usr/include/stdc-predef.h
