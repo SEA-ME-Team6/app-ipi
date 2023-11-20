9afa2c4dbfc104f393b21de0d248a89340fc05ba
K0Dahyun <gdh1685@kookmin.ac.kr>
2023-11-20 18:07:41 +0100

    fix error of gear qml

--- build/instrument_cluster/CMakeFiles/instrument_cluster.dir/instrument_cluster_autogen/UVLADIE3JM/qrc_qml.cpp.o.d
+++
@@ -1,3 +0,0 @@
-instrument_cluster/CMakeFiles/instrument_cluster.dir/instrument_cluster_autogen/UVLADIE3JM/qrc_qml.cpp.o: \
- /home/seame-workstation08/DES_Head-Unit/build/instrument_cluster/instrument_cluster_autogen/UVLADIE3JM/qrc_qml.cpp \
- /usr/include/stdc-predef.h
