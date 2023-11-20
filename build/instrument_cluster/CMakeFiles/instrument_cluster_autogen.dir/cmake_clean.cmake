94d0c520aa6469548f00868a5ea68a5c72a6909e
K0Dahyun <gdh1685@kookmin.ac.kr>
2023-11-20 17:49:52 +0100

    test v29

--- build/instrument_cluster/CMakeFiles/instrument_cluster_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/instrument_cluster_autogen"
-  "instrument_cluster_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/instrument_cluster_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
